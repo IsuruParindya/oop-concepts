34cec179315fd271624fac49297c448c6b41f955
IsuruParindya <isuruparindya2000@gmail.com>
2026-02-08 09:53:52 +0530

    Implement Customer-related classes and data storage functionality

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -14,6 +14,14 @@ class Customer {
     private String name;
     private String email;
 
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
     public Customer(String name, String email) {
         this.name = name;
         this.email = email;
