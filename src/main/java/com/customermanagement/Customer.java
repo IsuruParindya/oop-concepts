2db04919007fe8366e715d2a9502382e2c77d36f
Isuru Parindya Pigera <isuruparindya2000@gmail.com>
2026-02-13 16:26:52 +0530

    Update Customer.java
    Change the Customer.java
    
    Co-Authored-By: GihaChathura <261374942+gihachathura@users.noreply.github.com>

--- src/main/java/com/customermanagement/Customer.java
+++ src/main/java/com/customermanagement/Customer.java
@@ -1,16 +1,36 @@
 package com.customermanagement;
+
 public class Customer {
     private String name;
     private String email;
 
+    // Constructor
     public Customer(String name, String email) {
         this.name = name;
         this.email = email;
     }
+
+    // Getters
     public String getName() {
         return name;
     }
+
     public String getEmail() {
         return email;
+    }
+
+    // Setters (optional, if you want to allow changes)
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Override toString for easy printing
+    @Override
+    public String toString() {
+        return "Customer{name='" + name + "', email='" + email + "'}";
     }
 }
